c097e40713939b577e43da8fecc8150908dc6dcd
Alyssa Coghlan <ncoghlan@gmail.com>
2024-12-17 20:40:10 +1000

    Interim source tarball for Python 3.13 compatibility

--- versioneer.py
+++ versioneer.py
@@ -1449,74 +1449,23 @@ def get_versions(verbose=False):
         # see the discussion in cmdclass.py:get_cmdclass()
         del sys.modules["versioneer"]
 
-    root = get_root()
-    cfg = get_config_from_root(root)
+    # Override version info for a Python 3.13 compatible pre-release
+    # that can be built from a GitHub source tarball
+    #
+    # This is an interim workaround while waiting for the next upstream release:
+    #
+    # * https://github.com/jcrist/msgspec/issues/764
+    # * https://github.com/jcrist/msgspec/issues/777
+    #
+    # Hardcoding the version allows this to be built from a GitHub commit
+    # tarball rather than having to trust a third party build process
 
-    assert cfg.VCS is not None, "please set [versioneer]VCS= in setup.cfg"
-    handlers = HANDLERS.get(cfg.VCS)
-    assert handlers, "unrecognized VCS '%s'" % cfg.VCS
-    verbose = verbose or cfg.verbose
-    assert (
-        cfg.versionfile_source is not None
-    ), "please set versioneer.versionfile_source"
-    assert cfg.tag_prefix is not None, "please set versioneer.tag_prefix"
-
-    versionfile_abs = os.path.join(root, cfg.versionfile_source)
-
-    # extract version from first of: _version.py, VCS command (e.g. 'git
-    # describe'), parentdir. This is meant to work for developers using a
-    # source checkout, for users of a tarball created by 'setup.py sdist',
-    # and for users of a tarball/zipball created by 'git archive' or github's
-    # download-from-tag feature or the equivalent in other VCSes.
-
-    get_keywords_f = handlers.get("get_keywords")
-    from_keywords_f = handlers.get("keywords")
-    if get_keywords_f and from_keywords_f:
-        try:
-            keywords = get_keywords_f(versionfile_abs)
-            ver = from_keywords_f(keywords, cfg.tag_prefix, verbose)
-            if verbose:
-                print("got version from expanded keyword %s" % ver)
-            return ver
-        except NotThisMethod:
-            pass
-
-    try:
-        ver = versions_from_file(versionfile_abs)
-        if verbose:
-            print("got version from file %s %s" % (versionfile_abs, ver))
-        return ver
-    except NotThisMethod:
-        pass
-
-    from_vcs_f = handlers.get("pieces_from_vcs")
-    if from_vcs_f:
-        try:
-            pieces = from_vcs_f(cfg.tag_prefix, root, verbose)
-            ver = render(pieces, cfg.style)
-            if verbose:
-                print("got version from VCS %s" % ver)
-            return ver
-        except NotThisMethod:
-            pass
-
-    try:
-        if cfg.parentdir_prefix:
-            ver = versions_from_parentdir(cfg.parentdir_prefix, root, verbose)
-            if verbose:
-                print("got version from parentdir %s" % ver)
-            return ver
-    except NotThisMethod:
-        pass
-
-    if verbose:
-        print("unable to compute version")
-
+    override = "0.19.0.dev0+ncoghlan-py313-prerelease"
     return {
-        "version": "0+unknown",
-        "full-revisionid": None,
+        "version": override,
+        "full-revisionid": override,
         "dirty": None,
-        "error": "unable to compute version",
+        "error": None,
         "date": None,
     }
 
