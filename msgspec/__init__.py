66b7ab362369baac41217fccd79c133b132dde24
Alyssa Coghlan <ncoghlan@gmail.com>
2024-12-17 21:10:28 +1000

    Also override runtime API version

--- msgspec/__init__.py
+++ msgspec/__init__.py
@@ -29,7 +29,16 @@ from . import yaml
 from . import toml
 from . import inspect
 from . import structs
-from ._version import get_versions
 
-__version__ = get_versions()["version"]
-del get_versions
+# Override version info for a Python 3.13 compatible pre-release
+# that can be built from a GitHub source tarball
+#
+# This is an interim workaround while waiting for the next upstream release:
+#
+# * https://github.com/jcrist/msgspec/issues/764
+# * https://github.com/jcrist/msgspec/issues/777
+#
+# Hardcoding the version allows this to be built from a GitHub commit
+# tarball rather than having to trust a third party build process
+
+__version__ = "0.18.7.dev0+ncoghlan-py313-prerelease"
